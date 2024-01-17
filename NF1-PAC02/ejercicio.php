2350066210e9faef18a735cb25f655c8eb3971cb
sanchezreyesdaniel <120597815+sanchezreyesdaniel@users.noreply.github.com>
2024-01-17 18:00:31 +0100

    Add files via upload

--- NF1-PAC02/ejercicio.php
+++ NF1-PAC02/ejercicio.php
@@ -1,130 +1,134 @@
 <?php
+abstract class AbstractFileSystemElement
+{
+    private $name;
 
-// Clase base para elementos del sistema de archivos
-class FileSystemElement {
-    protected $name;
-
-    public function __construct($name) {
+    public function setName($name)
+    {
         $this->name = $name;
     }
 
-    public function getName() {
+    public function getName()
+    {
         return $this->name;
     }
 }
 
-// Clase para archivos
-class File extends FileSystemElement {
-    private $content;
-
-    public function __construct($name, $content) {
-        parent::__construct($name);
-        $this->content = $content;
-    }
-
-    public function getContent() {
-        return $this->content;
+class File extends AbstractFileSystemElement
+{
+    public function __construct($name)
+    {
+        parent::setName($name);
     }
 }
 
-// Clase para enlaces suaves
-class SoftLink extends FileSystemElement {
-    private $target;
+class Folder extends AbstractFileSystemElement
+{
+    private $files = [];
 
-    public function __construct($name, $target) {
-        parent::__construct($name);
-        $this->target = $target;
+    public function __construct($name)
+    {
+        parent::setName($name);
     }
 
-    public function getTarget() {
-        return $this->target;
-    }
-}
-
-// Clase para enlaces duros
-class HardLink extends FileSystemElement {
-    private $target;
-
-    public function __construct($name, $target) {
-        parent::__construct($name);
-        $this->target = $target;
+    public function add(AbstractFileSystemElement $file)
+    {
+        $this->files[] = $file;
     }
 
-    public function getTarget() {
-        return $this->target;
-    }
-}
-
-// Clase para carpetas
-class Folder extends FileSystemElement {
-    private $contents;
-
-    public function __construct($name) {
-        parent::__construct($name);
-        $this->contents = [];
+    public function getContent()
+    {
+        return count($this->files) > 0;
     }
 
-    public function addElement(FileSystemElement $element) {
-        $this->contents[] = $element;
-    }
-
-    public function getContents() {
-        return $this->contents;
-    }
-}
-
-// Ejemplo de uso
-$file1 = new File('archivo1.txt', 'Contenido del archivo 1');
-$file2 = new File('archivo2.txt', 'Contenido del archivo 2');
-$file3 = new File('archivo3.txt', 'Contenido del archivo 3');
-
-$softLink1 = new SoftLink('soft-link1.txt', $file1);
-$softLink2 = new SoftLink('soft-link2.txt', $file2);
-
-$hardLink1 = new HardLink('hard-link1.txt', $file1);
-$hardLink2 = new HardLink('hard-link2.txt', $file3);
-
-$folder = new Folder('carpeta_principal');
-$folder->addElement($file1);
-$folder->addElement($file2);
-$folder->addElement($softLink1);
-$folder->addElement($hardLink1);
-
-$subfolder = new Folder('subcarpeta');
-$subfolder->addElement($file3);
-$subfolder->addElement($softLink2);
-$subfolder->addElement($hardLink2);
-
-$folder->addElement($subfolder);
-
-// Puedes acceder a los elementos dentro de la carpeta así:
-$contents = $folder->getContents();
-
-// Puedes imprimir información sobre los elementos:
-foreach ($contents as $element) {
-    if ($element instanceof File) {
-        echo "Archivo: {$element->getName()} - Contenido: {$element->getContent()}\n";
-    } elseif ($element instanceof SoftLink) {
-        echo "Enlace suave: {$element->getName()} - Destino: {$element->getTarget()->getName()}\n";
-    } elseif ($element instanceof HardLink) {
-        echo "Enlace duro: {$element->getName()} - Destino: {$element->getTarget()->getName()}\n";
-    } elseif ($element instanceof Folder) {
-        echo "Carpeta: {$element->getName()}\n";
-        // Puedes acceder a los elementos dentro de la subcarpeta así:
-        
-        $subContents = $element->getContents();
-        foreach ($subContents as $subElement) {
-            if ($subElement instanceof File) {
-                echo "  Archivo: {$subElement->getName()} - Contenido: {$subElement->getContent()}\n";
-            } elseif ($subElement instanceof SoftLink) {
-                echo "  Enlace suave: {$subElement->getName()} - Destino: {$subElement->getTarget()->getName()}\n";
-            } elseif ($subElement instanceof HardLink) {
-                echo "  Enlace duro: {$subElement->getName()} - Destino: {$subElement->getTarget()->getName()}\n";
+    public function getFiles()
+    {
+        echo $this->getName() . "<br/>";
+        if ($this->getContent()) {
+            echo "<ul><li>Contenido de la carpeta es:</li><ul>";
+            foreach ($this->files as $file) {
+                if ($file instanceof Link) {
+                    echo $file->listAttributes();
+                } elseif ($file instanceof Folder) {
+                    $file->getFiles();
+                } else {
+                    echo "<li>" . $file->getName() . "</li>";
+                }
             }
-            echo "<br>";
+            echo "</ul></ul>";
         }
-        
     }
-    echo "<br>";
 }
-?>
+
+class Link extends AbstractFileSystemElement
+{
+    private $isSoftLink;
+    private $Linkto;
+
+    public function __construct($name, $isSoftLink, $Linkto)
+    {
+        parent::setName($name);
+        $this->isSoftLink = $isSoftLink;
+        $this->Linkto = $Linkto;
+    }
+
+    public function getisSoftLink()
+    {
+        return $this->isSoftLink ? "SoftLink" : "HardLink";
+    }
+
+    public function Path()
+    {
+        if (is_object($this->Linkto)) {
+            return $this->Linkto->getName();
+        } else {
+            return "No es un enlace";
+        }
+    }
+
+    public function getLinkto()
+    {
+        return $this->Path();
+    }
+
+    public function allAttributes()
+    {
+        return [
+            'Nombre' => $this->getName(),
+            'Tipo' => $this->getisSoftLink(),
+            'Linkto' => $this->getLinkto()
+        ];
+    }
+
+    public function listAttributes()
+    {
+        $attributesLink = $this->allAttributes();
+        echo "Información del enlace:";
+        echo "<ul>";
+        foreach ($attributesLink as $key => $value) {
+            echo "<li>$key: $value</li>";
+        }
+        echo "</ul>";
+    }
+}
+
+$file = new File("Archivo_Beita.jpg");
+$file2 = new File("Archivo_Nico.jpg");
+$folder = new Folder("Carpeta_Beita");
+$folder2 = new Folder("Carpeta_Dani");
+$folder3 = new Folder("Carpeta_Jordi");
+$link = new Link("Enlace_Beita", 1, $folder);
+$link2 = new Link("Enlace_Francesc", 0, $file);
+$link3 = new Link("Enlace_Roto", 1, "error");
+
+$folder->add(new File("Ramon.jpg"));
+$folder->add(new File("Ramon2.jpg"));
+$folder->add(new Link("Enlace_Beita", 1, $folder));
+$folder->add(new Link("Enlace_Francesc", 0, $file));
+$folder->add(new Folder("Carpeta de Ramon"));
+$folder2->add($folder);
+$folder2->add(new Link("Enlace_Roto", 1, "error"));
+$folder3->add($folder2);
+
+
+echo $folder3->getFiles();
